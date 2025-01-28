97e03345d49d33813de6c9c25cb668430dd381b3
ta-lita <66541239+ta-lita@users.noreply.github.com>
2025-01-28 17:37:21 -0300

    Add files via upload

---
+++ Básico/for.py
@@ -0,0 +1,8 @@
+for dia in ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]:
+    print(dia) #printa a variável dia com cada um dos itens da lista selecionada
+
+for numero in range(10): #de 0 até 99
+    print(numero, end=" ") #end é usado para escrever os resultados na mesma linha 
+
+for numero2 in range(0, 10, 2):
+    print(numero2, end=" ")
